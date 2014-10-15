e5137d94f82d0fb292ba6140fb1928775254358c
Thomas Rieder <thomas@rieder.io>
2014-10-15 14:58:26 +0200

    added vagrant config

---
+++ Vagrantfile
@@ -0,0 +1,25 @@
+Vagrant.configure("2") do |config|
+
+    # base box - ubuntu 14.04 lts
+    config.vm.box = "ubuntu/trusty64"
+
+    # networking:
+    #  9200 - rest api port
+    #  9300 - java api port
+    #  80:1080 - nodejs http port
+    #
+    config.vm.network :forwarded_port, guest:9200, host: 9200, auto_correct: true
+    config.vm.network :forwarded_port, guest:9300, host: 9300, auto_correct: true
+    config.vm.network :forwarded_port, guest:80, host: 1080, auto_correct: true
+
+    # set proper name and ram
+    config.vm.provider "virtualbox" do |v|
+        v.name = "Songster Development Vagrantbox"
+        v.customize ["modifyvm", :id, "--memory", "1024"]
+    end
+
+    # run our provision script to install elasticsearch
+    config.vm.provision "shell" do |s|
+        s.path = "misc/provision.sh"
+    end
+end
---
+++ misc/provision.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Going root"
+sudo -i
+
+echo "Updating package cache..."
+apt-get update > /dev/null
+
+echo "Installing Java..."
+apt-get install openjdk-7-jre-headless -y
+
+echo "Adding elasticsearch repo..."
+wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main
+" >> /etc/apt/sources.list
+apt-get update > /dev/null
+
+echo "Installing elasticsearch..."
+apt-get install elasticsearch -y
+
+echo "Adding elasticsearch to autostart..."
+update-rc.d elasticsearch defaults 95 10
+
+echo "Starting elasticsearch..."
+/etc/init.d/elasticsearch start
+
+echo "Installing elasticsearch-head plugin as a frontend..."
+/usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+
+echo "Adding MongoDB repo..."
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+apt-get -y update > /dev/null
+
+echo "Installing MongoDB..."
+apt-get install -y mongodb-10gen
+
+echo "Adding NodeJS repo..."
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get -y update > /dev/null
+
+echo "Installing node.js..."
+apt-get install -y nodejs
+
+echo "Finished provisioning. Elasticsearch: http://localhost:9200/_plugin/head/; NodeJS: http://localhost:1080"
