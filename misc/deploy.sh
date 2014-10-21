c219215a21cc4269454b5444746412a154fa1e98
Lisa Fichtinger <lisa@fichtinger.cc>
2014-10-21 23:25:28 +0200

    move deploy.sh into misc folder

---
+++ misc/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+		chmod 600 id_rsa
+        eval `ssh-agent -s`
+        ssh-add id_rsa
+        cat .travis/known_hosts >> ~/.ssh/known_hosts
+        git remote add dokku_main dokku@dev.songster-project.me:main
+        git push dokku_main master 
+    else
+        echo "not on master - no deployment started"
+    fi 
