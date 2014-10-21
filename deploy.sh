a557f93dcd3df5dbf6dfa70a8adf4e520acb01fc
Lisa Fichtinger <lisa@fichtinger.cc>
2014-10-21 21:30:01 +0200

    travis config - added bash script deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+    chmod 600 id_rsa
+    eval `ssh-agent -s`
+    ssh-add id_rsa
+    cat .travis/known_hosts >> ~/.ssh/known_hosts
+    git remote add dokku_main dokku@dev.songster-project.me:main
+    git push dokku_main master
