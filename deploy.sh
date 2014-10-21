83cf9d1d83fe061dce8f2a0b012bdb9e4e79d69a
Lisa Fichtinger <lisa@fichtinger.cc>
2014-10-21 23:17:07 +0200

    using travis branch env variable works

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-    branch_name=$(git symbolic-ref -q HEAD)
-    branch_name=${branch_name##refs/heads/}
-    branch_name=${branch_name:-HEAD}
-
-    echo $branch_name
-
-    bname="$(git symbolic-ref HEAD 2>/dev/null)" ||
-    bname="(unnamed branch)"
-
-    bname=${bname##refs/heads/}
-    echo $bname
-
-    echo $TRAVIS_BRANCH
-
-    if [ "$bname" = "master" ]; then
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
 		chmod 600 id_rsa
         eval `ssh-agent -s`
         ssh-add id_rsa
