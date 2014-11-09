4b5e2a1aa0a7b41244c8382e78220c83094b8203
Markus Zisser <markus.zisser@gmx.at>
2014-11-09 23:35:59 +0100

    added example test

--- tests/DummyTest.js
+++ tests/DummyTest.js
@@ -1,10 +1,26 @@
-var expect = require('chai').expect;
+// our assertion library
+var chai = require('chai');
+// sinon integration for chai
+var sinonChai = require("sinon-chai");
+// sinon is a spy, stub, and mocking framework for javascript
+var sinon = require("sinon");
+chai.use(sinonChai);
+var expect = chai.expect;
 
-/**
- * just a dummy test case that passes every time
- */
-describe('dummy Test ',function(){
-    it('should pass',function(){
-        expect(true).to.equal(true);
+
+describe('dummy Test', function () {
+    it('ensureAuthenticated calls next if user is authenticated', function () {
+        var pass = require('./../config/passport');
+        var req = {
+            isAuthenticated: function () {
+                return true;
+            }
+        };
+        var res = {};
+        res.redirect = function () {
+        };
+        var next = sinon.spy();
+        pass.ensureAuthenticated(req, res, next);
+        expect(next).to.have.been.called;
     })
 })
