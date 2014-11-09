1e05490f18e5883ca650bfce5cd4612f008d551a
Markus Zisser <markus.zisser@gmx.at>
2014-11-09 20:45:00 +0100

    added a dummy test case

---
+++ tests/DummyTest.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+/**
+ * just a dummy test case that passes every time
+ */
+describe('dummy Test ',function(){
+    it('should pass',function(){
+        expect(true).to.equal(true);
+    })
+})
