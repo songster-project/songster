2bdeb74c9796455997b8f95cc56ddcbe60c7e8ba
psaeuerl <p.saeuerl@aon.at>
2014-11-09 22:51:16 +0100

    casing of words, fixed rating

--- config/database.js
+++ config/database.js
@@ -22,12 +22,13 @@ var userSchema = new Schema({
 
 var songSchema = new Schema({
         owner_id: ObjectId, //Referencing to user
-        addeddate:  { type: Date, default: Date.now },
-        file_id: ObjectId, //Referencing to the fileid in the files collection
+        addedDate:  { type: Date, default: Date.now },
+        file_id: ObjectId, //Referencing to the file_id in the files collection
         active: { type: Boolean, default: true},
         type: {type: String, enum: songTypes},
-        rating: {type: Number, min:0, max:5}, //must be full value
-        //one of both will be filled
+        rating: {type: Number, min:1, max:5, default: null}, //must be integer value
+
+        //one of them will be filled
         filename: String,
         url: String,
 
@@ -54,22 +55,23 @@ var eventSchema = new Schema({
     link: String,
     start: { type: Date, default: Date.now},
     end: { type: Date, default: null},
-    votingenabled: Boolean,
-    previewenabled: Boolean,
-    suggestionenabled: Boolean
+    votingEnabled: Boolean,
+    previewEnabled: Boolean,
+    suggestionEnabled: Boolean
 
 }, {collection: 'event'});
 
 var voteSchema = new Schema({
     owner_id: ObjectId, //Referencing to users, might be null
     type: {type: String, enum: voteTypes},
+    state: {type: string, enum: voteStates},
     song_id: ObjectId, //Referencing to the song suggested
     event_id: ObjectId //Referencing to the event that this vote was posted
 }, {collection: 'vote'});
 
 var eventlogSchema = new Schema({
     event_id: ObjectId, //Referencing to the event that log comes from
-    logdate: {type: Date, default: Date.now},
+    logDate: {type: Date, default: Date.now},
     message: {} //Any Json object you want to log
 }, {collection: 'eventlog'});
 
