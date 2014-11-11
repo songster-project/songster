0b62d5efaf18d3e9b0745ca4d9dc1d7ba0350265
Thomas Rieder <tho.rieder@aon.at>
2014-11-11 15:58:52 +0100

    fix deploy.sh to do force pushes

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -6,7 +6,7 @@
         ssh-add id_rsa
         cat .travis/known_hosts >> ~/.ssh/known_hosts
         git remote add dokku_main dokku@dev.songster-project.me:main
-        git push dokku_main master 
+        git push dokku_main master --force
     else
         echo "not on master - no deployment started"
     fi 
