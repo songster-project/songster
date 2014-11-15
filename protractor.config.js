983366bf753550fbabfa35736c723c109b9310a1
Markus Zisser <markus.zisser@gmx.at>
2014-11-15 11:25:55 +0100

    added selenium server address to protractor

--- protractor.config.js
+++ protractor.config.js
@@ -1,4 +1,6 @@
 exports.config ={
+    // The address of a running selenium server.
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs:['./system_tests/*.js'],
     framework:'jasmine',
     capabilities:{
