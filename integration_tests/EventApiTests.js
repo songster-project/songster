6d9684b7e8a146ba6a64ab3b493de30402013461
psaeuerl <p.saeuerl@aon.at>
2014-11-17 16:56:09 +0100

    event api tests

--- integration_tests/EventApiTests.js
+++ integration_tests/EventApiTests.js
@@ -24,41 +24,91 @@ describe('Test', function () {
     //Logged In
     //#########################################################################################
 
+    it('should return no events in the start of the tests', function (done) {
+        api.get('/event').end(function (err, res) {
+            expect(err).to.not.exist;
+            expect(res.body).to.be.empty;
+            done();
+        });
+    });
 
     it('should respond with the event when i post a valid event', function (done) {
             var postdata = {
                 "name": "myEvent",
-                "accessKey" : "theKey",
-                "owner_id" : "54638a8a8b5aca5d121cd09c",
-                "suggestionEnabled" : "true",
-                "votingEnabled" : "true",
-                "previewEnabled" : "true"
+                "accessKey": "theKey",
+                "owner_id": "54638a8a8b5aca5d121cd09c",
+                "suggestionEnabled": "true",
+                "votingEnabled": "true",
+                "previewEnabled": "true"
             }
             api.post('/event').send(postdata).end(function (err, res) {
                 expect(err).to.not.exist;
                 expect(res.body).to.contain.key('start');
+                eventid = res.body._id;
                 done();
-            })
+            });
         }
     );
 
     it('should throw an error if i create an second event', function (done) {
-            var postdata = {
-                "name": "myEvent",
-                "accessKey" : "theKey",
-                "owner_id" : "54638a8a8b5aca5d121cd09c",
-                "suggestionEnabled" : "true",
-                "votingEnabled" : "true",
-                "previewEnabled" : "true"
-            }
-            api.post('/event').send(postdata).end(function (err, res) {
-                console.log(res);
-                console.log(err);
-                expect(res.error.text).to.equal('This user has already an event running');
-                expect(res.error.status).to.equal(400);
-                done();
-            })
+        var postdata = {
+            "name": "myEvent",
+            "accessKey": "theKey",
+            "owner_id": "54638a8a8b5aca5d121cd09c",
+            "suggestionEnabled": "true",
+            "votingEnabled": "true",
+            "previewEnabled": "true"
         }
-    );
+        api.post('/event').send(postdata).end(function (err, res) {
+                api.post('/event').send(postdata).end(function (err, res) {
+                    expect(res.error.text).to.equal('This user has already an event running');
+                    expect(res.error.status).to.equal(400);
+                    done();
+                })
+            }
+        );
 
+
+    });
+
+    it('should show the event as the active event', function (done) {
+        api.get('/event/active').end(function (err, res) {
+            expect(err).to.not.exist;
+            expect(res.body).to.have.length(1);
+            expect(res.body[0].name).to.equal('myEvent');
+            done();
+        });
+    });
+
+
+    it('should show the event as the current event', function (done) {
+        api.get('/event/current').end(function (err, res) {
+            expect(err).to.not.exist;
+            expect(res.body.name).to.equal('myEvent');
+            done();
+        });
+    });
+
+
+    it('should be able to get the current event via id', function (done) {
+        api.get('/event/current').end(function (err, res) {
+
+            var id = res.body._id;
+            api.get('/event/'+id).end(function(err,res) {
+                expect(err).to.not.exist;
+                expect(res.body.name).to.equal('myEvent');
+                done();
+            });
+        });
+    });
+
+    it('should set the end date when ending the event', function (done) {
+        api.put('/event/current/end').send({}).end(function (err,res)
+        {
+            console.log("ending");
+            expect(err).to.not.exist;
+            expect(res.body.end).to.not.equal(null);
+            done;
+        });
+    });
 });
