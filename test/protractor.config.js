1c60b58f6e282dbbd1b98997ee3c05dc9754cdd6
Markus Zisser <markus.zisser@gmx.at>
2014-11-17 15:35:23 +0100

    restructured tests

--- test/protractor.config.js
+++ test/protractor.config.js
@@ -1,7 +1,7 @@
 exports.config ={
     // The address of a running selenium server.
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs:['./test/system/**/*.js'],
+    specs:['./system/**/*.js'],
     framework:'jasmine',
     capabilities:{
         browserName: 'firefox'
