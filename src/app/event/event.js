be95cb9bddfcb5aff6cf97482afd5ab4aea0f0c8
psaeuerl <p.saeuerl@aon.at>
2014-11-23 18:09:51 +0100

    autoformat ... also setting of standard values

--- src/app/event/event.js
+++ src/app/event/event.js
@@ -15,53 +15,59 @@ angular.module('songster.event')
     })
 
 
-    .controller('EventCtrl', function EventCtrl($scope,  $http) {
+    .controller('EventCtrl', function EventCtrl($scope, $http) {
 
-       // $scope.eventActive = false;
+        // $scope.eventActive = false;
         //First we check if we have an event ...
         $http.get('/event/current')
             .success(function (data) {
                 $scope.event = data;
-                if(_.isEmpty(data)  ) {
+                if (_.isEmpty(data)) {
                     $scope.eventActive = false;
                     //Set standard values
-                    $scope.event.votingEnabled = true;
-                    $scope.event.suggestionEnabled = true;
-                    $scope.event.previewEnabled = false;
+                    $scope.setStandardValuesForEvent();
                 }
                 else {
                     $scope.eventActive = true;
                 }
             });
 
-        $scope.startEvent = function() {
+        $scope.startEvent = function () {
             //ToDo: acceessKey ... look if we really need it
             //also ... owner_id is currently hardcoded to user1
             $scope.event.accessKey = '1234';
-            $scope.event.owner_id = "546b16fa2e3a10ea162d9355";
-            $http.post('/event', $scope.event).
-                success(function(data, status, headers, config) {
-                    $scope.eventActive = true;
-                    $scope.event = data;
+            $http.get('/account/id').success(function (data) {
+                $scope.event.owner_id = data.id;
+                $http.post('/event', $scope.event).
+                    success(function (data, status, headers, config) {
+                        $scope.eventActive = true;
+                        $scope.event = data;
+                    }).
+                    error(function (data, status, headers, config) {
+                        $scope.eventErrors = data;
+                    });
+            });
+        };
+
+        $scope.endEvent = function () {
+            $http.put('/event/current/end', {}).
+                success(function (data, status, headers, config) {
+                    $scope.eventActive = false;
+                    $scope.event = {};
+                    $scope.setStandardValuesForEvent();
                 }).
-                error(function(data, status, headers, config) {
+                error(function (data, status, headers, config) {
                     $scope.eventErrors = data;
                 });
         };
 
-        $scope.endEvent = function() {
-            $http.put('/event/current/end', {}).
-                success(function(data, status, headers, config) {
-                    $scope.eventActive = false;
-                    $scope.event = {};
-                    $window.location.href = '/event';
-                }).
-                error(function(data, status, headers, config) {
-                   $scope.eventErrors = data;
-                });
+        $scope.setStandardValuesForEvent = function () {
+            $scope.event.votingEnabled = true;
+            $scope.event.suggestionEnabled = true;
+            $scope.event.previewEnabled = false;
         };
 
 
-    })
+    });
 
-;
+
