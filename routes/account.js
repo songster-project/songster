dc421e275368582ccba59c63a853cb367c317492
psaeuerl <p.saeuerl@aon.at>
2014-11-23 18:09:08 +0100

    added route to account to get my account id

--- routes/account.js
+++ routes/account.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../config/passport');
 
+//PS: avoids 304 in this module
+//Problem occurs when i want to request the /id, and it doesn't change
+router.get('/*', function(req, res, next){
+    res.setHeader('Last-Modified', (new Date()).toUTCString());
+    next();
+});
+
 router.get('/', passport.ensureAuthenticated, function (req, res) {
     res.render('account', { id: req.session.passport.user });
 });
+
+
+router.get('/id', passport.ensureAuthenticated, function(req,res) {
+    res.status(200).send({ id: req.session.passport.user});
+})
 
 module.exports = router;
