94412ef7e414ed76310637277d08fdb8da8825ed
Manuel Geier <manuel.geier@catalysts.com>
2014-12-02 02:11:17 +0100

    cleanup search code

--- routes/search.js
+++ routes/search.js
@@ -8,33 +8,13 @@ var client = new elasticsearch.Client({
     log: 'trace'
 });
 
-var index = 'songster';
-var publicFields = ['title', 'artist', 'album'];
-
 var getSourceElementsFromResponse = function (response) {
     return response;
 };
 
-//router.get('/', passport.ensureAuthenticated, function (req, res) {
-//    client.search({
-//        index: 'users',
-//        size: 50,
-//        body: {
-//            query: {
-//                match: {
-//                    profile: 'elasticsearch'
-//                }
-//            }
-//        }
-//    }).then(function (resp) {
-//        var hits = resp.body.hits;
-//    });
-//    res.send('This is the search!');
-//});
-
 router.get('/song', passport.ensureAuthenticated, function (req, res) {
     client.search({
-        index: index,
+        index: 'songster',
         type: 'song',
         pretty: true
     }, function (error, response) {
@@ -47,7 +27,7 @@ router.get('/song', passport.ensureAuthenticated, function (req, res) {
 
 router.get('/song/:query', passport.ensureAuthenticated, function (req, res) {
     client.search({
-        index: index,
+        index: 'songster',
         type: 'song',
         q: '_all:' + req.params.query
     }, function (error, response) {
@@ -57,38 +37,5 @@ router.get('/song/:query', passport.ensureAuthenticated, function (req, res) {
     });
 
 });
-
-//router.get('/song/album/:name', passport.ensureAuthenticated, function (req, res) {
-//    client.search({
-//        index: 'songs',
-//        q: 'album:' + req.params.name
-//    }, function (error, response) {
-//        if (!error) {
-//            res.send(getSourceElementsFromResponse(response));
-//        }
-//    });
-//});
-//
-//router.get('/song/artist/:name', passport.ensureAuthenticated, function (req, res) {
-//    client.search({
-//        index: 'songs',
-//        q: 'artist:' + req.params.name
-//    }, function (error, response) {
-//        if (!error) {
-//            res.send(getSourceElementsFromResponse(response));
-//        }
-//    });
-//});
-//
-//router.get('/song/year/:name', passport.ensureAuthenticated, function (req, res) {
-//    client.search({
-//        index: 'songs',
-//        q: 'year:' + req.params.name
-//    }, function (error, response) {
-//        if (!error) {
-//            res.send(getSourceElementsFromResponse(response));
-//        }
-//    });
-//});
 
 module.exports = router;
