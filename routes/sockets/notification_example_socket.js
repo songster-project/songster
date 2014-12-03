23fdfcb8bf0c0af9a2842921d5b888233924893f
Markus Zisser <markus.zisser@gmx.at>
2014-12-03 22:41:30 +0100

    removed test output and unnecessary code

--- routes/sockets/notification_example_socket.js
+++ routes/sockets/notification_example_socket.js
@@ -2,11 +2,9 @@ var nserver = require('./../../lib/notification_server');
 var clients = new Array();
 
 nserver.register_Event('lala', function (ws,req, payload) {
-    console.log('callback');
     nserver.send_Notifications('lala',payload,clients);
 });
 
 nserver.register_User('lala', function (ws,req, payload) {
-    console.log('new user');
     clients.push(ws);
 });
