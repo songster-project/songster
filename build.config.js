330b9e1a85c2f47cfe7c9ba9e7c3906654fab3a0
Thomas Rieder <thomas@rieder.io>
2014-12-05 00:05:12 +0100

    disable karma crap and enable grunt watch

--- build.config.js
+++ build.config.js
@@ -7,7 +7,7 @@ module.exports = {
    * development and the `compile_dir` folder is where our app resides once it's
    * completely built.
    */
-  build_dir: 'build',
+  build_dir: 'app',
   compile_dir: 'app',
 
   /**
