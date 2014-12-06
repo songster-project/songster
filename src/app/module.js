8d2fc16db1e5ac23dd31f724111c82fbcdf8c945
Manuel Geier <manuel.geier@catalysts.com>
2014-12-06 10:52:31 +0100

    created domain objects module for frontend and added song domain object

--- src/app/module.js
+++ src/app/module.js
@@ -4,6 +4,7 @@ angular.module('ngBoilerplate', [
     'ngBoilerplate.home',
     'ngBoilerplate.about',
     'ngBoilerplate.upload',
+    'songster.domain',
     'songster.menu',
     'songster.main-menu',
     'songster.player',
