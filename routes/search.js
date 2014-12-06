8ced4bb7bd507bb2903d6b73810f739ba093047a
Manuel Geier <manuel.geier@catalysts.com>
2014-12-06 11:54:59 +0100

    elastic search song search is now restricted to the current user

--- routes/search.js
+++ routes/search.js
@@ -6,21 +6,57 @@ var elasticSearchService = require('../backend/services/elasticSearchService');
 router.get('/song', passport.ensureAuthenticated, function (req, res) {
     elasticSearchService.getClient().search({
         index: 'songster',
-        type: 'song'
+        type: 'song',
+        body: {
+            "query": {
+                "filtered": {
+                    "filter": {
+                        "term": {
+                            "owner_id": req.user._id
+                        }
+                    }
+                }
+            }
+        }
     }, function (error, response) {
         if (!error) {
             res.send(response);
         }
     });
-
 });
+
+
+
+
+
 
 router.get('/song/:query', passport.ensureAuthenticated, function (req, res) {
     var escapedQuery = elasticSearchService.escape(req.params.query);
     elasticSearchService.getClient().search({
         index: 'songster',
         type: 'song',
-        q: '_all:' + escapedQuery
+        body: {
+            "query": {
+                "filtered": {
+                    "query": {
+                        "multi_match": {
+                            "query": escapedQuery,
+                            "fields": [
+                                "title",
+                                "artist",
+                                "album",
+                                "year"
+                            ]
+                        }
+                    },
+                    "filter": {
+                        "term": {
+                            "owner_id": req.user._id
+                        }
+                    }
+                }
+            }
+        }
     }, function (error, response) {
         if (!error) {
             res.send(response);
