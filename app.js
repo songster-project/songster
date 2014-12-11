5ce89b7be59430b61ea58c1ae07fe0a9ebba99c3
Lisa Fichtinger <lisa@fichtinger.cc>
2014-12-11 10:22:53 +0100

    fixed routing problem of voting after rebasing

--- app.js
+++ app.js
@@ -75,7 +75,6 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/voting/:id',passportinit.redirectVoting);
 app.use('/app', passportinit.ensureAuthenticated, express.static(path.join(__dirname, 'app')));
 app.use('/login', login);
 app.use('/logout', logout);
