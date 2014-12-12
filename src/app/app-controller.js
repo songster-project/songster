e6f1ce8648210682077861ed2ea686ad0783e53a
Manuel Geier <manuel.geier@catalysts.com>
2014-12-12 23:00:13 +0100

    add authentication methods to rootScope to be globally available

--- src/app/app-controller.js
+++ src/app/app-controller.js
@@ -1,7 +1,14 @@
 angular
     .module('ngBoilerplate')
 
-    .controller('AppCtrl', function AppCtrl($scope, $location) {
+    .controller('AppCtrl', function AppCtrl($scope, $rootScope, $location, $auth) {
+
+        $rootScope.isAuthorized = function() {
+            return $auth.isAuthorized();
+        };
+
+        $rootScope.isAnonymous = function() {
+            return $auth.isAnonymous();
+        };
 
     });
-
