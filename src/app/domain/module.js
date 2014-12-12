d5fc3e4cb033aa47918c92f1d50e3631aaa79975
Lisa Fichtinger <lisa@fichtinger.cc>
2014-12-12 21:03:40 +0100

    adapted domain modules

--- src/app/domain/module.js
+++ src/app/domain/module.js
@@ -1,5 +1,9 @@
 angular.module('songster.domain.song', []);
+angular.module('songster.domain.postingVote', []);
+angular.module('songster.domain.receivedVote', []);
 
 angular.module('songster.domain', [
-        'songster.domain.song'
+        'songster.domain.song',
+        'songster.domain.postingVote',
+        'songster.domain.receivedVote'
     ]);
