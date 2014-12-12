26677b57d2509fb4cb9221aab9dfcc08ea4cfdff
Lisa Fichtinger <lisa@fichtinger.cc>
2014-12-12 19:47:45 +0100

    added route in search to get songs from event owner in backend

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../config/passport');
 var elasticSearchService = require('../backend/services/elasticSearchService');
+var db = require('../config/database');
 
 router.get('/song', passport.ensureAuthenticated, function (req, res) {
     elasticSearchService.getClient().search({
@@ -61,6 +62,58 @@ router.get('/song/:query', passport.ensureAuthenticated, function (req, res) {
         if (!error) {
             res.send(response);
         }
+    });
+
+});
+
+router.get('/eventsongs/:eventid/:query', passport.ensureAuthenticated, function (req, res) {
+
+    console.log('in search eventsongs');
+    // get active event with eventid from req.param
+    db.Event.findOne({_id: req.param('eventid'), end:null}, function (err, event) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal server error');
+            return;
+        }
+
+        if (event == null) {
+            console.log('event not found');
+            res.status(400).send('Bad Request');
+            return;
+        }
+
+        var escapedQuery = elasticSearchService.escape(req.params.query);
+        elasticSearchService.getClient().search({
+            index: 'songster',
+            type: 'song',
+            body: {
+                "query": {
+                    "filtered": {
+                        "query": {
+                            "multi_match": {
+                                "query": escapedQuery,
+                                "fields": [
+                                    "title",
+                                    "artist",
+                                    "album",
+                                    "year"
+                                ]
+                            }
+                        },
+                        "filter": {
+                            "term": {
+                                "owner_id": event.owner_id
+                            }
+                        }
+                    }
+                }
+            }
+        }, function (error, response) {
+            if (!error) {
+                res.send(response);
+            }
+        });
     });
 
 });
