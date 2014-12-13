b7fff11485ac816b741a66f839701cac98e04f90
Markus Zisser <markus.zisser@gmx.at>
2014-12-13 23:34:01 +0100

    removed unused event-song module

--- src/app/event-songs/config.js
+++ src/app/event-songs/config.js
@@ -1,23 +1,6 @@
 angular
     .module('songster.event-songs')
 
-    .config(function config($stateProvider) {
-        $stateProvider.state('event-songs', {
-            url: '/event-songs',
-            views: {
-                "main": {
-                    controller: 'event-songsCtrl',
-                    templateUrl: 'event-songs/event-songs.tpl.html'
-                }
-            },
-            data: {pageTitle: 'Event Songs'}
-        });
-    })
-
-    .config(['$menuProvider', function ($menuProvider) {
-        //$menuProvider.addMenuEntry('main', 'Event_songs', 'fa-bell', 'event-songs', 400);
-    }])
-
     .run(function config($rootScope, eventService) {
         // TODO this needs to be removed
         $rootScope.$on('BROADCAST_STARTED', function (event /*event of the listener*/, broadcastEvent) {
