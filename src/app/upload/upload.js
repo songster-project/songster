d2f9a4a11864ecbe0d381fb3a640cd275f3a2939
Markus Zisser <markus.zisser@gmx.at>
2014-12-14 16:14:29 +0100

    added frontend for youtube upload

--- src/app/upload/upload.js
+++ src/app/upload/upload.js
@@ -3,29 +3,6 @@ angular.module('ngBoilerplate.upload')
     .controller('UploadCtrl', function UploadCtrl($scope, $upload, $http) {
         $scope.uploadedFiles = [];
 
-        $scope.sendUrl = function () {
-            var msg = {
-                youtubeurl: $scope.youtubeurl
-            };
-            $scope.uploadedFiles.push({
-                name: $scope.youtubeurl,
-                finished: false
-            });
-            $http.post('/youtube/', msg)
-                .success(function (data) {
-                    // file is uploaded successfully
-                    for (var i = 0; i < $scope.uploadedFiles.length; i++) {
-                        if ($scope.uploadedFiles[i].name === $scope.youtubeurl) {
-                            $scope.uploadedFiles.splice(i, 1);
-                        }
-                    }
-                    $scope.uploadedFiles.push({
-                        name: $scope.youtubeurl,
-                        finished: true
-                    });
-                });
-        };
-
         $scope.onFileSelect = function ($files) {
             // $files: an array of files selected, each file has name, size, and type.
             for (var i = 0; i < $files.length; i++) {
