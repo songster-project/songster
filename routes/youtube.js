cf6d6e9551518a93c9592a51aac7a3166101c757
Markus Zisser <markus.zisser@gmx.at>
2014-12-14 20:58:19 +0100

    use domain for error handling

--- routes/youtube.js
+++ routes/youtube.js
@@ -6,61 +6,52 @@ var elasticSearchService = require('../backend/services/elasticSearchService');
 var ytdl = require('ytdl-core');
 var Ffmpeg = require('fluent-ffmpeg');
 var util = require('util');
-
+var domain = require('domain');
+//song will be cut after this time (with format [[hh:]mm:]ss[.xxx])
+var MAX_SONG_DURATION = '10:00';
 
 router.post('/', passport.ensureAuthenticated, passport.ensureNotAnonymous, function (req, res) {
-    try {
-        //variable used to end execution if there is a error during steaming
-        //needed because of the nested function calls
-        //TODO if there is a better way change it
-        var streamfailed = false;
-        req.checkBody('youtubeurl', 'URL is empty').notEmpty();
-        var errors = req.validationErrors();
-        if (errors) {
-            res.status(400).send('There have been validation errors: ' + util.inspect(errors));
-            return;
-        }
 
-        ytdl.getInfo(req.body.youtubeurl, function (err, info) {
-            //if error occurs or youtube link is not valid send status 400
-            if (err || info.title === undefined) {
-                console.log(err);
-                res.status(400).send('Bad request');
+    try {
+        var d = domain.create();
+        d.on('error', function (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
+        d.run(function () {
+            req.checkBody('youtubeurl', 'URL is empty').notEmpty();
+            var errors = req.validationErrors();
+            if (errors) {
+                res.status(400).send('There have been validation errors: ' + util.inspect(errors));
                 return;
             }
-            var title = info.title;
-            //create stream to download from youtube (with 380p video quality)
-            var ytdlstream = ytdl(req.body.youtubeurl, {quality: 18})
-                //if error occurs during video downlaod send status 500
-                .on('error', function (err) {
-                    if (!streamfailed) {
-                        console.log(err);
-                        streamfailed = true;
-                        res.status(500).send('Internal Server Error');
-                    }
-            });
-            //create stream to conver youtube video stream into mp3 stream
-            var ffmpegstream = new Ffmpeg({source: ytdlstream})
-                .withAudioCodec('libmp3lame')
-                .toFormat('mp3')
-                .duration('10:00')
-                .on('error', function (err) {
-                    if (!streamfailed) {
-                        console.log(err);
-                        streamfailed = true;
-                        res.status(500).send('Internal Server Error');
-                    }
-                });
-            //create stream to stream song into database
-            var writeStream = database.gfs.createWriteStream({
-                mode: 'w',
-                filename: title + '.mp3'
-            });
 
-            //pipe mp3 stream into database
-            ffmpegstream.pipe(writeStream);
-            writeStream.on('close', function () {
-                if (!streamfailed) {
+            ytdl.getInfo(req.body.youtubeurl, function (err, info) {
+                //if error occurs or youtube link is not valid send status 400
+                if (err || info.title === undefined) {
+                    console.log(err);
+                    res.status(400).send('Bad request');
+                    return;
+                }
+                var title = info.title;
+                //create stream to download from youtube (with 380p video quality)
+                var ytdlstream = ytdl(req.body.youtubeurl, {quality: 18});
+
+                //create stream to conver youtube video stream into mp3 stream
+                var ffmpegstream = new Ffmpeg({source: ytdlstream})
+                    .withAudioCodec('libmp3lame')
+                    .toFormat('mp3')
+                    .duration(MAX_SONG_DURATION);
+
+                //create stream to stream song into database
+                var writeStream = database.gfs.createWriteStream({
+                    mode: 'w',
+                    filename: title + '.mp3'
+                });
+
+                //pipe mp3 stream into database
+                ffmpegstream.pipe(writeStream);
+                writeStream.on('close', function () {
                     var metadata = {
                         addedDate: new Date(),
                         title: title || '',
@@ -76,7 +67,7 @@ router.post('/', passport.ensureAuthenticated, passport.ensureNotAnonymous, func
                         elasticSearchService.indexSong(record);
                         res.status(200).send('OK');
                     });
-                }
+                });
             });
         });
         //sometimes the error didn't get caught on the on error listeners
