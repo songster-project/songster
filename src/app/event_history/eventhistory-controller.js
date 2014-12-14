9b28275235b600367bc405176e8d11cfee86dd83
psaeuerl <p.saeuerl@aon.at>
2014-12-14 01:41:08 +0100

    basic - showing event names and clicking + updating

--- src/app/event_history/eventhistory-controller.js
+++ src/app/event_history/eventhistory-controller.js
@@ -1,5 +1,10 @@
 angular.module('songster.eventhistory')
 
     .controller('EventHistoryController', function EventCtrl($scope, $http ) {
-
+        $scope.events = [{name: "1"},{ name: "2"}];
+        $scope.event={};
+        
+        $scope.showEvent = function(event) {
+            $scope.event = event;
+        }
     });
