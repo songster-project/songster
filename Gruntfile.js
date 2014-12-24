ab2ce412eefd7d08c9be5c002b37e211de9e7b3f
Manuel Geier <manuel.geier@catalysts.com>
2014-12-24 16:59:46 +0100

    - created a grunt tast `test` to only run tests (more or less everything which is in build)  - added some additional reporters for the frontend test results (results can be found in /out/test/frontend/ and in the console)  - package.json is now ordered (was done automaticially)

--- Gruntfile.js
+++ Gruntfile.js
@@ -577,6 +577,13 @@ module.exports = function ( grunt ) {
     'less:compile', 'copy:compile_assets', 'ngmin', 'concat:compile_js',/* 'uglify',*/ 'index:compile'
   ]);
 
+  grunt.registerTask( 'test', [
+    'clean', 'html2js', 'less:build',
+    'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
+    'copy:build_appjs', 'copy:build_vendorjs',
+    'index:build', 'karmaconfig', 'karma:continuous'
+  ] );
+
   /**
    * A utility function to get all app JavaScript sources.
    */
