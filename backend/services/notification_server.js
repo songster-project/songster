0cf8161a0508a77582fd34e2753fae52d06d69f9
Lisa Fichtinger <lisa@fichtinger.cc>
2014-12-27 22:03:28 +0100

    fixes ttl index error on collection sessions of notification server

--- backend/services/notification_server.js
+++ backend/services/notification_server.js
@@ -26,7 +26,8 @@ module.exports = function () {
             host: settings.db_host,
             port: settings.db_port,
             username: settings.db_user,
-            password: settings.db_pass
+            password: settings.db_pass,
+            auto_reconnect: true
         }),
         saveUninitialized: true,
         resave: true
