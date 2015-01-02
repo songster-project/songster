1e3b243498b886da1f8e042fff0fe3f3f7479c14
Lisa Fichtinger <lisa@fichtinger.cc>
2015-01-02 18:22:58 +0100

    add get route usersuggestions route at voting adapted select in find vote to return all information

--- routes/voting.js
+++ routes/voting.js
@@ -47,7 +47,7 @@ var votesWs = require('../backend/websockets/votes_suggests');
 
 
 /**
- * returns and array of all votes from the event with given event_id that have not been played yet
+ * returns and array of all votes and suggests from the event with given event_id that have not been played yet
  * [{ song_id: { title: 'xxxx', album: 'xxxx', artist: 'xxx', year: '2000', _id: xx },
  *   state: 'new'
   *  type: 'vote',
@@ -84,8 +84,7 @@ router.get('/votedsongs/:eventid', passport.ensureAuthenticated, function(req, r
         }
 
         db.Vote.find( {event_id: event._id, state: {$ne: 'played'}})
-            .select( 'date state type song_id')
-            .populate( {path: 'song_id', model: 'Song', select: '_id title artist album year'})
+            .populate( {path: 'song_id', model: 'Song', select: '_id title artist album year owner_id'})
             .exec( function(err, votes){
                 if(err) {
                     console.log(err);
@@ -140,7 +139,6 @@ router.get('/uservotes/:eventid', passport.ensureAuthenticated, function(req, re
 
         db.Vote.find({event_id: event._id, owner_id: req.user.id, state: 'new'})
             .populate({path: 'song_id', model: 'Song', select: '_id title artist album year'})
-            .select( 'song_id type -_id')
             .exec( function(err, votes) {
                 if (err) {
                     console.log(err);
@@ -154,6 +152,62 @@ router.get('/uservotes/:eventid', passport.ensureAuthenticated, function(req, re
     });
 });
 
+/**
+ * returns an array of suggestions
+ * from the current user from the event with given event_id that have not been played yet
+ * [ { song_id:,
+ *     event_id:
+ *     date: ,
+ *     suggestion_type: {type: String, enum: songTypes},
+ *     video_id: String
+ *    },
+ *  { song_id:, ... } ]
+ *
+ * used for disabling multiple votes from a user
+ * the client aggregates those song_ids
+ */
+router.get('/usersuggestions/:eventid', passport.ensureAuthenticated, function(req, res){
+
+    req.checkParams('eventid', 'Event ID must not be empty').notEmpty();
+    req.checkParams('eventid', 'event_id is not an ID').isMongoID();
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.status(400).send('There have been validation errors: ' + util.inspect(errors));
+        return;
+    }
+
+    // get current event with eventid
+    db.Event.findOne({_id: req.param('eventid'), end:null}, function (err, event) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal server error');
+            return;
+        }
+
+        if(event == null) {
+            console.log('event not found');
+            res.status(400).send('Bad Request');
+            return;
+        }
+
+        db.Vote.find({event_id: event._id, owner_id: req.user.id, type:'suggestion'})
+            .populate({path: 'song_id', model: 'Song', select: '_id title artist'})
+            .exec( function(err, suggestions) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Internal server error');
+                    return;
+                }
+                res.status(200).send(suggestions);
+                return;
+            });
+
+    });
+});
+
+
+
 
 router.post('/:event_id', passport.ensureAuthenticated, function(req, res) {
 
@@ -163,6 +217,15 @@ router.post('/:event_id', passport.ensureAuthenticated, function(req, res) {
     req.checkParams('event_id', 'Event ID must not be empty').notEmpty();
     req.checkBody('song_id', 'Song_id is not an ID').isMongoID();
     req.checkParams('event_id', 'event_id is not an ID').isMongoID();
+
+    if(req.param('type') === 'suggestion') {
+        req.assert('suggestion_type', 'Type of suggest does not match type file or youtube').isInArray(('file youtube').split(' '));
+        if(req.param('suggestion_type') === 'youtube') {
+            req.checkBody('video_id', 'video id of youtube suggestion must not be empty').notEmpty();
+        }
+    }
+
+    console.log(req.body);
 
     var errors = req.validationErrors();
     if (errors) {
@@ -201,7 +264,7 @@ router.post('/:event_id', passport.ensureAuthenticated, function(req, res) {
             }
 
             // search for votes on song and event from current user
-            db.Vote.find({event_id: event._id, owner_id: req.user.id, song_id: song._id, state: 'new', type: 'vote'}, function(err, votes) {
+            db.Vote.find({event_id: event._id, owner_id: req.user.id, song_id: song._id, state: 'new'}, function(err, votes) {
                 if(err) {
                     console.log(err);
                     res.status(500).send('Internal server error');
@@ -220,6 +283,8 @@ router.post('/:event_id', passport.ensureAuthenticated, function(req, res) {
                 vote.state = req.body.state;
                 vote.song_id = req.body.song_id;
                 vote.event_id = req.param('event_id');
+                vote.suggestion_type = req.param('suggestion_type');
+                vote.video_id = req.param('video_id');
 
                 vote.save( function(err, vote){
                     if(err)  {
@@ -232,7 +297,7 @@ router.post('/:event_id', passport.ensureAuthenticated, function(req, res) {
                     // notify web socket vote_changed event
 
                     db.Vote.findOne( {_id: vote._id})
-                        .select( 'date state type song_id')
+                        .select( 'date state type song_id suggestion_type')
                         .populate( {path: 'song_id', model: 'Song', select: '_id title artist album year'})
                         .exec( function(err, vote_pop){
                             if(err) {
@@ -242,6 +307,7 @@ router.post('/:event_id', passport.ensureAuthenticated, function(req, res) {
                         });
 
 
+                    //TODO adapt eventlog to save suggestions vote.type === suggestion
                     db.Song.find({_id: vote.song_id}, function (err, song) {
                         // save the vote to the event log
                         if (song && song.length > 0) {
