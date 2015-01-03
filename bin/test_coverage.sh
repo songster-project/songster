33861f7986f18086c70472308820862bcc47a1a0
Markus Zisser <markus.zisser@gmx.at>
2015-01-03 21:25:06 +0100

    changed coverage test script for integration tests

--- bin/test_coverage.sh
+++ bin/test_coverage.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-echo "_____ starting backend tests _____"
-./node_modules/mocha/bin/mocha -R dot $(find test/unit/backend -name '*.js')
-echo "_____ backend testing finished _____"
-
-echo "_____ starting frontend tests _____"
-grunt karma:continuous
-echo "_____ frontend testing finished _____"
-
-echo "_____ starting server _____"
-sudo Xvfb :10 -ac &  > /dev/null
-export DISPLAY=:10  > /dev/null
 
 echo "_____ setting up testdata _____"
 ./bin/importdb > /dev/null
 
+echo "_____ starting server _____"
+npm start &
+sleep 20
+
 echo "_____ starting integration tests _____"
 ./node_modules/mocha/bin/mocha -R dot $(find test/integration -name '*.js')
 echo "_____ integration testing finished _____"
-
-echo "_____ starting system tests _____"
-./node_modules/protractor/bin/protractor test/localprotractor.config.js
-echo "_____ system tests finished _____"
