8017e227eec7605113825883358e78d464e9228c
Markus Zisser <markus.zisser@gmx.at>
2015-01-05 11:33:29 +0100

    added coverage for backend unit tests

--- bin/test_coverage.sh
+++ bin/test_coverage.sh
@@ -5,8 +5,11 @@ echo "_____ setting up testdata _____"
 
 echo "_____ starting server _____"
 npm start &
-sleep 20
+sleep 10
+
+echo "_____ running grunt _____"
+grunt > /dev/null
 
 echo "_____ starting integration tests _____"
-./node_modules/mocha/bin/mocha -R dot $(find test/integration -name '*.js')
+./node_modules/mocha/bin/mocha -R List $(find test/integration -name '*.js')
 echo "_____ integration testing finished _____"
